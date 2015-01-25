da12e1e490f31448466d2aaace6163a699fdd0e5
siukaido <siukaido@gmail.com>
2015-01-25 18:26:22 +0900

    - add test init

---
+++ tests/ArrayHashTest.php
@@ -0,0 +1,13 @@
+<?php
+
+class ArrayHashTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+    }
+
+    public function test_fail()
+    {
+        $this->fail();
+    }
+}
